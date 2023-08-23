773a4b06e22b251f7b51888ced5fae457f7a8bd7
aminakodzaga <amina.kodzaga@stu.ibu.edu.ba>
2023-08-23 20:14:42 +0200

    sorting dates

--- assets/js/appointment-service.js
+++ assets/js/appointment-service.js
@@ -4,15 +4,13 @@ var AppointmentService = {
             submitHandler: function(form) {
                 var entity = Object.fromEntries((new FormData(form)).entries());
                 AppointmentService.add(entity);
-                
 
             }
         });
-       
-
+ 
     },
-
     
+
     list: function(){
         $.get("rest/appointment", function(data) {
           $("#appointment-list").html("");
@@ -76,6 +74,58 @@ var AppointmentService = {
         });
     },
 
+
+    
+    listSortedByDate: function(sortOrder) {
+      $.get("rest/appointment", function(data) {
+          if (sortOrder === "asc") {
+              data.sort((a, b) => a.date.localeCompare(b.date));
+          } else if (sortOrder === "desc") {
+              data.sort((a, b) => b.date.localeCompare(a.date)); // Descending order
+          }
+
+          $("#appointment-list").html(""); // Clear the appointment list container
+          var html = "";
+          for (let i = 0; i < data.length; i++) {
+              // Generate HTML for each appointment and append to 'html' variable
+              html += `
+              <div class="col-lg-3">
+              <div class="card" >
+                <img class="card-img-top" >
+                <div class="card-body">
+                  <h5 class="card-title">`+ data[i].date +`</h5>
+                  <h5>`+data[i].name+`</h5>
+                  <h5>`+data[i].doctor_id+`</h5>
+                 
+                 
+                </div>
+              </div>
+            </div>
+              `;
+          }
+          $("#appointment-list").html(html); // Update the appointment list container with sorted HTML
+      });
+  },
+  setupSorting: function() {
+    var sortOrder = "asc"; // Initial sorting order is ascending
+
+    $("#sort-by-date").click(function() {
+        sortOrder = (sortOrder === "asc") ? "desc" : "asc"; // Toggle sorting order
+        AppointmentService.listSortedByDate(sortOrder);
+    });
+}
+};
+
+$(document).ready(function() {
+AppointmentService.init();
+AppointmentService.setupSorting(); // Call the sorting setup function here
+})
+
+
+  
+
+
+
+
     
 
-}
